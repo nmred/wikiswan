c5fab058dd53eb9586633bf2e9a0b19c4c4f86a4
nmred <nmred_2008@126.com>
2013-01-27 20:53:43 +0800

    add wiki

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+nmred:$1$yvJQw5LV$l4fxRLM1yq3tk9BMvlnKu0:nmred:nmred_2008@126.com:admin,user
